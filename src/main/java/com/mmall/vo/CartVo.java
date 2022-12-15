264f04af703af67b769de18903a3d11f7cc3c0e3
guolin <guolin1633@163.com>
2022-12-15 17:57:36 +0800

    用Lombok优化代码

--- src/main/java/com/mmall/vo/CartVo.java
+++ src/main/java/com/mmall/vo/CartVo.java
@@ -1,47 +1,20 @@
 package com.mmall.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 /**
  * Created by guolin
  */
+@Getter
+@Setter
 public class CartVo {
 
     private List<CartProductVo> cartProductVoList;
     private BigDecimal cartTotalPrice; // 购物车的总价
     private Boolean allChecked; // 是否已经都勾选
     private String imageHost; // 图片前缀http://img.happymmall.com/
-
-    public List<CartProductVo> getCartProductVoList() {
-        return cartProductVoList;
-    }
-
-    public void setCartProductVoList(List<CartProductVo> cartProductVoList) {
-        this.cartProductVoList = cartProductVoList;
-    }
-
-    public BigDecimal getCartTotalPrice() {
-        return cartTotalPrice;
-    }
-
-    public void setCartTotalPrice(BigDecimal cartTotalPrice) {
-        this.cartTotalPrice = cartTotalPrice;
-    }
-
-    public Boolean getAllChecked() {
-        return allChecked;
-    }
-
-    public void setAllChecked(Boolean allChecked) {
-        this.allChecked = allChecked;
-    }
-
-    public String getImageHost() {
-        return imageHost;
-    }
-
-    public void setImageHost(String imageHost) {
-        this.imageHost = imageHost;
-    }
 }
